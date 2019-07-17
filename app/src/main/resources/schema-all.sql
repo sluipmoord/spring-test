e02d93c0099089906451ed1d1d191457d65b7b01
Gerhard Janse van Rensburg <gerhard@teamgeek.co.za>
2019-07-17 11:19:39 +0200

    made some batch processing stuff

--- app/src/main/resources/schema-all.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS people;
-
-CREATE TABLE people  (
-    person_id int(11) NOT NULL auto_increment,
-    first_name varchar(20),
-    last_name varchar(20),
-    PRIMARY KEY (`person_id`)
-);
